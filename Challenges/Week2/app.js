7474d1e2781018b81dca33886d914c9f8bd8d8fa
samstl77 <acutty@hotmail.com>
2016-09-08 20:06:38 -0400

    Weekly JS challenge for week 2

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,24 @@
 //create function
   //control and return statements
 
-//Prompt user for input and store variable
+function processInput(message)
+{
+  console.log("i'm inside of the function");
+  if(message == "Hello!")
+  console.log("Hello World");
+  else {
+    return("you didn't say hello :(");
+  }
+}
+var userInput;
+
+userInput = prompt();
 
 //Call function
-
+console.log(message);
 //Alert user results
+
+
+var ressponse = processInput(userInput);
+
+console.log (ressponse);
